2ed8b4a522d7b3efb44a54abe2444408e0a98cc0
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2012-02-14 11:53:49 +0200

    add script for keeping dotfiles elsewhere, symlinked

---
+++ create-symlinks.sh
@@ -0,0 +1,11 @@
+--2012-02-14 11:52:23--  http://paste.pocoo.org/raw/kWoenX3kVknu19fQnxWK/
+Resolving paste.pocoo.org... 188.40.116.183
+Connecting to paste.pocoo.org|188.40.116.183|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/plain]
+Saving to: `index.html'
+
+     0K .                                                      33.2M=0s
+
+2012-02-14 11:52:24 (33.2 MB/s) - `index.html' saved [1359]
+
