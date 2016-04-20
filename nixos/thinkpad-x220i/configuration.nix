b3f857892b9bb4355c21ccfb726cc03ca975113c
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-04-20 16:52:01 +0300

    add hdaps

--- nixos/thinkpad-x220i/configuration.nix
+++ nixos/thinkpad-x220i/configuration.nix
@@ -101,13 +101,20 @@
   # TLP Linux Advanced Power Management
   services.tlp.enable = true;
 
+  # hard disk protection if the laptop falls
+  services.hdapsd.enable = true;
+
   # trackpoint support (touchpad disabled in this config)
   hardware.trackpoint.enable = true;
   hardware.trackpoint.emulateWheel = true;
 
-  # fingerprint reader: use X with fingerprint (if you add one with `fprintd-enroll`)
-  services.fprintd.enable = true;
-  security.pam.services.slim.fprintAuth = true;
-  security.pam.services.xscreensaver.fprintAuth = true;
+  # alternatively, touchpad with two-finger scrolling
+  #services.xserver.libinput.enable = true;
+
+  # fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
+  #services.fprintd.enable = true;
+  #security.pam.services.login.fprintAuth = true;
+  #security.pam.services.xscreensaver.fprintAuth = true;
+  # similarly for other PAM providers
 }
 # vim: et:sw=2:ts=2:ai
