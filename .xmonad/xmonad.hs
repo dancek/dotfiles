c185e6a8e9c82455964b8f9b0568f69ca37c2380
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-05-11 18:21:16 +0300

    auto-launch xmobar with xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,14 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Util.Run(spawnPipe)
 
 main = do
-    -- xmproc <- spawnPipe "/path/to/xmobar"
+    xmproc <- spawnPipe "/run/current-system/sw/bin/xmobar /home/dance/.xmobarrc"
     xmonad $ defaultConfig
-        { modMask = mod4Mask
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , modMask = mod4Mask
         , terminal = "urxvt"
         } `additionalKeysP`
         [ ("<XF86ScreenSaver>", spawn "xscreensaver-command -lock")
