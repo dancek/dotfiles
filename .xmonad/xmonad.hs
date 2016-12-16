7af5e49cbebe6ef90c052850b1add1283ea0de01
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-12-16 16:14:21 +0200

    better xmobar support (Super-Shift-f toggles bar)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,13 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.NoBorders(smartBorders)
 
 main = do
     xmproc <- spawnPipe "/run/current-system/sw/bin/xmobar /home/dance/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
         , modMask = mod4Mask
         , terminal = "urxvt"
         -- workaround for java programs (java has a hard-coded WM list with settings)
@@ -16,4 +17,5 @@ main = do
         } `additionalKeysP`
         [ ("<XF86ScreenSaver>", spawn "xscreensaver-command -lock")
         , ("<XF86Launch1>", spawn "chromium") -- ThinkVantage button
+        , ("M-S-f", sendMessage ToggleStruts)
         ]
