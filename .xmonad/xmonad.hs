690f55e45a52404af8249dc55254df4fa6bd2631
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-12-17 16:20:45 +0200

    use updatePointer to move mouse when focus changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,12 +4,16 @@ import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Layout.NoBorders(smartBorders)
+import XMonad.Actions.UpdatePointer(updatePointer)
 
 main = do
     xmproc <- spawnPipe "/run/current-system/sw/bin/xmobar /home/dance/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+        -- when new window is focused, move pointer inside window
+        -- (new xmonad-contrib 0.12 syntax)
+        , logHook = updatePointer (0.5, 0.5) (0.8, 0.8)
         , modMask = mod4Mask
         , terminal = "urxvt"
         -- workaround for java programs (java has a hard-coded WM list with settings)
