c7e7ed0dbeb1dfd130e73d3e5a223eeb37b4f036
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-12-22 11:09:24 +0200

    TLP CPU power saving

--- nixos/thinkpad-x220i/configuration.nix
+++ nixos/thinkpad-x220i/configuration.nix
@@ -110,6 +110,8 @@
     DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
     START_CHARGE_THRESH_BAT0=85
     STOP_CHARGE_THRESH_BAT0=90
+    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    ENERGY_PERF_POLICY_ON_BAT=powersave
   '';
 
   # hard disk protection if the laptop falls
